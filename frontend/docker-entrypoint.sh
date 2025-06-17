36d0909d44448c35d6400cf8165a6df52c6b4aa6
rishab-gupta19 <rishab-gupta19>
2025-06-17 16:48:39 +0530

    github actions workflow.6

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -6,6 +6,6 @@
 # passed at Docker run time, is used in the Nginx configuration.
 envsubst '$GKE_BACKEND_IP' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Execute the original command passed to the entrypoint (e.g., "nginx -g 'daemon off;'")
+# Execute the original command passed to the entrypoint (e.g "nginx -g 'daemon off;'")
 # This ensures Nginx starts correctly after the config is updated.
 exec "$@"
