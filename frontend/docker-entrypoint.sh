63f873d550b35984f0a1b8dfb33893d9e143a2a6
rishab-gupta19 <rishab-gupta19>
2025-06-17 16:46:26 +0530

    Github actions workflow.5

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Substitute environment variables into the Nginx config template
 # and output the result to the actual Nginx config file.
-# This ensures that the GKE_BACKEND_IP environment variable,
+# This ensures that the GKE_BACKEND_IP environment variable
 # passed at Docker run time, is used in the Nginx configuration.
 envsubst '$GKE_BACKEND_IP' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
